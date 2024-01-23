c1e6a7c55571fed1fdb4796461d26bdd7ea891b9
junaidhashim6 <junaidhashim6@gmail.com>
2024-01-23 15:10:34 +0500

    state and shallow merge in class components

--- Monsters-Rolodex/src/App.jsx
+++ Monsters-Rolodex/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+import { Component } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: "Junaid", secondName: "Hashim" },
+    };
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>
+          Hello {this.state.name.firstName} {this.state.name.secondName}{" "}
+        </h1>
+        <button
+          onClick={() => {
+            this.setState(
+              () => {
+                return {
+                  name: { firstName: "John", secondName: "Smith" },
+                };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+          }}
+        >
+          Change Name
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
+    );
+  }
 }
 
-export default App
+export default App;

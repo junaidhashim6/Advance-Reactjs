4937daa5dfb880f5ed33cba51663dacc26297a1b
junaidhashim6 <junaidhashim6@gmail.com>
2024-01-26 15:40:24 +0500

    lifecycle method componentDidMount

--- Monsters-Rolodex/src/App.jsx
+++ Monsters-Rolodex/src/App.jsx
@@ -4,26 +4,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        { name: "Linda" },
-        { name: "Frank" },
-        { name: "Jacky" },
-        { name: "Andrei" },
-      ],
+      monsters: [],
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
   }
   render() {
     return (
       <>
         <div className="App">
-         {this.state.monsters.map((monster, id)=>{
-          return(
-            <h1 key={id}>{monster.name}</h1>
-          ) 
-         })}
+          {this.state.monsters.map((monster) => {
+            return(
+              <div key={monster.id}>
+              <h1 >{monster.name}</h1>
+              <h2 >{monster.email}</h2>
+              </div>
+            ) 
+            
+          })}
         </div>
-        
-        
       </>
     );
   }

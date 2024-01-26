e72c22aad41d4a0ff7e2af3fd6799fab69d4fa33
junaidhashim6 <junaidhashim6@gmail.com>
2024-01-26 13:03:43 +0500

    apply map filter on an array

--- Monsters-Rolodex/src/App.jsx
+++ Monsters-Rolodex/src/App.jsx
@@ -1,44 +1,29 @@
 import { Component } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Junaid", secondName: "Hashim" },
+      monsters: [
+        { name: "Linda" },
+        { name: "Frank" },
+        { name: "Jacky" },
+        { name: "Andrei" },
+      ],
     };
   }
   render() {
     return (
       <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+        <div className="App">
+         {this.state.monsters.map((monster, id)=>{
+          return(
+            <h1 key={id}>{monster.name}</h1>
+          ) 
+         })}
         </div>
-        <h1>
-          Hello {this.state.name.firstName} {this.state.name.secondName}{" "}
-        </h1>
-        <button
-          onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: { firstName: "John", secondName: "Smith" },
-                };
-              },
-              () => {
-                console.log(this.state);
-              }
-            );
-          }}
-        >
-          Change Name
-        </button>
+        
+        
       </>
     );
   }

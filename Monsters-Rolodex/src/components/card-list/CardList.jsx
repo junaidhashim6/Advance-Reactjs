bd7d67054c8081a36f3ce9e63e9d704c6897bbc7
junaidhashim6 <junaidhashim6@gmail.com>
2024-01-31 19:49:50 +0500

    add styles to cards

--- Monsters-Rolodex/src/components/card-list/CardList.jsx
+++ Monsters-Rolodex/src/components/card-list/CardList.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react/prop-types */
 import { Component } from "react";
+import "../card-list/card-list.css";
 class CardList extends Component {
   render() {
-    const { monsters} = this.props;
+    const { monsters } = this.props;
 
     return (
       <div className="card-list">
-       {monsters.map((monster)=>{
-        const {email, name , id} = monster;
-        return(
+        {monsters.map((monster) => {
+          const { email, name, id } = monster;
+          return (
             <div className="card-container" key={id}>
-            <img src={`https://robohash.org/${id}?set=set2`} alt={`monster ${name}`} />
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
-        )
-       })}
+              <img
+                src={`https://robohash.org/${id}?set=set2`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
